4f8f4f5d4613c130c9395b30d7b0c249d5ad8ef8
Jerald Tonmoy Dias <tonmoyjareld@gmail.com>
2022-09-26 01:16:45 +0600

    dynamic routing done

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,9 +1,13 @@
 import Layout from '../../components/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
+import Date from '../../components/date';
+import utilStyles from '../../styles/utils.module.css';
+import Head from 'next/head';
 
 
 export async function getStaticProps({ params }) {
-    const postData = getPostData(params.id);
+    const postData = await getPostData(params.id);
+
     return {
         props: {
             postData,
@@ -23,11 +27,16 @@ export async function getStaticPaths() {
 export default function Post({ postData }) {
     return (
         <Layout>
-            {postData.title}
-            <br />
-            {postData.id}
-            <br />
-            {postData.date}
+            <Head>
+                <title>{postData.title}</title>
+            </Head>
+            <article>
+                <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+                <div className={utilStyles.lightText}>
+                    <Date dateString={postData.date} />
+                </div>
+                <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+            </article>
         </Layout>
     );
 }
